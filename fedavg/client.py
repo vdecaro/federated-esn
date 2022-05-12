2086fe24fcb45d33342af4f939adf33da0d70130
vdecaro <valeriodc95@hotmail.it>
2022-05-12 15:25:19 +0200

    shuffle added

--- fedavg/client.py
+++ fedavg/client.py
@@ -91,5 +91,5 @@ class FedAvgClient:
         
         self.dataset.seq_length = config['SEQ_LENGTH']
 
-        self.loader = DataLoader(self.dataset, batch_size=config['BATCH_SIZE'], collate_fn=seq_collate_fn)
+        self.loader = DataLoader(self.dataset, batch_size=config['BATCH_SIZE'], shuffle=True, collate_fn=seq_collate_fn)
         return True
