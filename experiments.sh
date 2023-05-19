1f42e3adfe6777f8c2d641d392709e1c762239ac
vdecaro <43961074+vdecaro@users.noreply.github.com>
2023-05-19 12:50:28 +0200

    chore: Add experiments and analysis

--- experiments.sh
+++ experiments.sh
@@ -1,9 +1,9 @@
 # !/bin/sh
+python main.py HHAR  25  1
+python main.py HHAR  50  1
+python main.py HHAR  75  1
+python main.py HHAR  100 1
 python main.py WESAD 25  1
 python main.py WESAD 50  1
 python main.py WESAD 75  2
 python main.py WESAD 100 2
-python main.py HHAR  25  1
-python main.py HHAR  50  1
-python main.py HHAR  75  2
-python main.py HHAR  100 2
